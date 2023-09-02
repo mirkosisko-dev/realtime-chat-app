751f635ac8d9801735bbe520d53699b55b819023
mirkosisko-dev <mirko.sisko@gmail.com>
2023-09-02 15:37:35 +0200

    Installed dependencies, added core ui components and libs

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import Button from "@/components/ui/Button";
+import { FC } from "react";
+
+interface pageProps {}
+
+const page: FC<pageProps> = ({}) => {
+  return <div>Dashboard</div>;
+};
+
+export default page;
